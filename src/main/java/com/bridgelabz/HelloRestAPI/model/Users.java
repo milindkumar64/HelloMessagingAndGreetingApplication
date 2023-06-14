a5cf84bb5b2ad9c06744a0beb04a1462072504d5
milindkumar64 <milindgupta@gmail.com>
2023-06-14 12:35:13 +0530

    Added: Return Jason Using Greeting controller

--- src/main/java/com/bridgelabz/HelloRestAPI/model/Users.java
+++ src/main/java/com/bridgelabz/HelloRestAPI/model/Users.java
@@ -1,15 +1,12 @@
 package com.bridgelabz.HelloRestAPI.model;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Entity
 @Data // = getter + setter + ToString
-public class User {
+public class Users {
 
     @Id
     private String firstName;
